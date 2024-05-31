ad7211131ddef5340d286e3ab009c73de64b4b83
TommyBui89 <126430838+TommyBui89@users.noreply.github.com>
2024-05-31 19:18:37 +1000

    new website

--- src/App.js
+++ src/App.js
@@ -1,60 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import './styles.css';
+import { ThemeProvider } from "styled-components";
+import { useState, useEffect } from "react";
+import { darkTheme, lightTheme } from './utils/Themes.js'
+import Navbar from "./components/Navbar";
+import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import MenuBar from './Components/MenuBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import RoutesConfig from './Route';
-import MainBackground from './Resources/MainBackground.jpeg';
-import ParticlesBackground from './Components/ParticlesBackground';
+import HeroSection from "./components/HeroSection";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import ProjectDetails from "./components/ProjectDetails";
+import styled from "styled-components";
 
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 function App() {
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 600);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const containerStyle = {
-        // display: 'flex',
-        width: '100vw',
-        height: '100vh',
-        backgroundImage: `url(${MainBackground})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        flexDirection: 'column',
-        overflow: 'hidden',  // prevents potential scrolling issues
-        minHeight: '100vh'
-    };
-    
-    const innerDivStyle = {
-        backdropFilter: 'blur(10px)',
-        border: '1px solid rgba(255, 255, 255, 0.2)',
-        boxShadow: '0 0 20px rgba(0, 0, 0, 0.1)',
-        borderRadius: '10px',
-        padding: isMobile ? '10px' : '20px',
-        margin: isMobile ? '20px' : '40px',
-        height: isMobile ? 'calc(100vh - 2 * (20px + 10px))' : 'calc(100vh - 80px)', 
-        overflowY: 'auto' 
-    };      
-
-    return (
-        <div style={containerStyle}>
-            <ParticlesBackground />
-            <div style={innerDivStyle}>
-                <Router>
-                    <MenuBar />
-                    <RoutesConfig />
-                </Router>
-            </div>
-        </div>
-    );
+  const [darkMode, setDarkMode] = useState(true);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal)
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router >
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Experience />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Education />
+          </Wrapper>
+          <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
+        </Body>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, LinkedInButton, ButtonContainer, MobileMenu, MobileNavLogo, MobileLink } from './NavbarStyledComponent'
+import { Bio } from '../../data/constants';
+import { useTheme } from 'styled-components';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const theme = useTheme();
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to='/'>
+          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+            <Span>Tommy Bui</Span>
+          </a>
+        </NavLogo>
+        <NavItems>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href='#skills'>Skills</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
+          <NavLink href='#projects'>Projects</NavLink>
+          <NavLink href='#education'>Education</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <LinkedInButton href={Bio.linkedin} target="_blank">LinkedIn Profile</LinkedInButton>
+        </ButtonContainer>
+        {
+          isOpen &&
+          <MobileMenu isOpen={isOpen}>
+            <MobileLink href="#about" onClick={() => {
+              setIsOpen(!isOpen)
+            }}>About</MobileLink>
+            <MobileLink href='#skills' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Skills</MobileLink>
+            <MobileLink href='#experience' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Experience</MobileLink>
+            <MobileLink href='#projects' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Projects</MobileLink>
+            <MobileLink href='#education' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Education</MobileLink>
+            <LinkedInButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.linkedin} target="_blank">LinkedIn Profile</LinkedInButton>
+          </MobileMenu>
+        }
+      </NavbarContainer>
+    </Nav>
+  )
+}
+
+export default Navbar;
